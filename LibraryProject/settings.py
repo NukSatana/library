6a2b2cea24d5b41786633b88235e59a137f9b76a
NukSatana <49380114+NukSatana@users.noreply.github.com>
2023-06-12 01:39:37 +0300

    UploadPython2

--- LibraryProject/settings.py
+++ LibraryProject/settings.py
@@ -77,9 +77,6 @@ WSGI_APPLICATION = 'LibraryProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 DATABASES = {
     'default': {
@@ -87,7 +84,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
